efad03e5ae4b0f199ce51e21de83252933868d01
Jaehong Hwang <dev.jaehong@gmail.com>
2020-03-23 18:02:03 +0900

    vimrc 복사되도록 수정

--- init.sh
+++ init.sh
@@ -9,5 +9,8 @@ read -r -s -p "[sudo] sudo password for $(whoami):" pass
 
 brew bundle --file=./Brewfile
 
+hyper i hyper-tab-enhanced hyperline hyper-hypest
+cp .vimrc ../.vimrc
+
 printf '\n\n🎉 Congrat! Your mac has been set up successfully for working with PlusTV Team!\n'
 
